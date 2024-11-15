f0b50c077449034717db4b77c395bdedf2825f87
daguttt <dagutmu667@gmail.com>
2024-11-15 11:28:12 -0500

    feat: associate post creation with the current signed in user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_post, only: %i[ show edit update destroy ]
 
   # GET /posts or /posts.json
@@ -21,7 +22,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params.merge(status: :published))
 
     respond_to do |format|
       if @post.save
@@ -65,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :content, :status, :published_at, :user_id, :category_id)
+      params.require(:post).permit(:title, :content, :category_id)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum :role, [ :admin, :writer ]
+  has_many :posts
+
+  validates :name, presence: true
 end
