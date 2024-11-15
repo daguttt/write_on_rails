6b3a3cd34811fae09a98b27e8ca373597d49f4f9
daguttt <dagutmu667@gmail.com>
2024-11-15 11:30:58 -0500

    feat: add post model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   enum :status, [ :draft, :pending, :published, :unpublished ]
   belongs_to :user
   belongs_to :category
+
+  validates :title, :content, :status, presence: true
+  validates :title, length: { maximum: 255 }
+  validates :content, length: { minimum: 15 }
+  validates :status, inclusion: { in: %w[draft pending published unpublished] }
 end
