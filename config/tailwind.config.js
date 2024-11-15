8a30122279d42e8aacebc27cfb1c10a3b560ee37
daguttt <dagutmu667@gmail.com>
2024-11-15 11:31:41 -0500

    fix: add `main-min-h` utility class to tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,11 +1,12 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin');
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
@@ -18,5 +19,12 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
-}
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.main-min-h': {
+          minHeight: `calc(100svh - ${theme('spacing.4')} * 2)`,
+        },
+      });
+    }),
+  ],
+};
