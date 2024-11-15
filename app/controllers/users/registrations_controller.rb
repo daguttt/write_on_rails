ea82484297e910cf21593f2cd153440008b061cd
daguttt <dagutmu667@gmail.com>
2024-11-15 11:05:09 -0500

    feat: set writer as default role when signing up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  append_before_action :configure_sign_up_params, only: [ :create ]
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :role ])
+    # Sets default role when signing up
+    params[:user][:role] = :writer
+  end
+
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
